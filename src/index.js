ee4d7e863bae76c3a82ba8d58f273153ea504c07
Hans Schroeder <hajschroeder@gmail.com>
2022-03-06 10:44:55 -0800

    initial full commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import './css/styles.css';
+
+const test = [1,2,3,4,5,666];
+console.log(test);
+function testFunct(){
+  let myVar="car";
+  return myVar;
+}
+console.log(testFunct());
